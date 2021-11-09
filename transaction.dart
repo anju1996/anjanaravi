9b78e3aa92b4daf255e2683606ea7dbf90118df6
Anjana Ravi <32459573+anju1996@users.noreply.github.com>
2021-11-09 09:47:53 +0530

    Update transaction.dart

--- transaction.dart
+++ transaction.dart
@@ -1,6 +1,5 @@
 class Transaction {
   DateTime? createDate;
-  String? typeOfTransaction;
-  // double? amount;
-  Transaction({this.createDate, this.typeOfTransaction});
+  double? amount;
+  String? type;
 }
