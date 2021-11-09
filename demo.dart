63aeea4d70e3216d882fd0732b47adc923dd7a64
Anjana Ravi <32459573+anju1996@users.noreply.github.com>
2021-11-09 09:47:04 +0530

    Update demo.dart

--- demo.dart
+++ demo.dart
@@ -3,11 +3,12 @@ import "account.dart";
 import 'transaction.dart';
 
 class Bank {
-  bool? status;
+  // bool? status;
 
   final List<Account> list = [];
   Account account = Account();
-  Transaction transactions = Transaction();
+  Transaction transaction = Transaction();
+  // var transactionList = [];
 
   void openAccount() {
     print("Enter account number to create account");
@@ -16,6 +17,10 @@ class Bank {
     account.name = stdin.readLineSync()!;
     print("Enter an amount to create your account");
     account.balance = double.parse(stdin.readLineSync()!);
+    transaction.createDate = DateTime.now();
+    transaction.amount = account.balance;
+    transaction.type = "credit";
+    account.transactions.add(transaction);
     account.status = true;
     print("----Account is created----");
   }
@@ -35,12 +40,15 @@ class Bank {
       }
       print("enter the amount to credit");
       var amount = double.parse(stdin.readLineSync()!);
-      transactions.createDate = DateTime.now();
-      // transactions.amount = amount;
+      transaction.createDate = DateTime.now();
+      transaction.amount = amount;
+      transaction.type = "credit";
+
+      account.transactions.add(transaction);
       account.balance = mainBalance + amount;
-      // account.transactions.add(transactions);
+
       print(
-          "Your account is credited with $amount on ${transactions.createDate} and total balance is ${account.balance}");
+          "Your account is credited with $amount on ${transaction.createDate} and total balance is ${account.balance}");
     } else {
       print("----Your account is closed----");
     }
@@ -53,8 +61,8 @@ class Bank {
         mainBalance = account.balance!;
       }
       if (account.accountNumber != null && account.accountNumber != 0) {
-        int acn = account.accountNumber!;
-        print("Your account number is $acn");
+        int accountNumber = account.accountNumber!;
+        print("Your account number is $accountNumber");
       } else {
         print("enter the account number");
         account.accountNumber = int.parse(stdin.readLineSync()!);
@@ -65,15 +73,36 @@ class Bank {
       if (amount > account.balance!) {
         print("Insufficient Balance");
       } else {
-        transactions.createDate = DateTime.now();
-        // transactions.amount = amount;
+        transaction.createDate = DateTime.now();
+        transaction.amount = amount;
+        transaction.type = "debit";
+        account.transactions.add(transaction);
         account.balance = mainBalance - amount;
-        // account.transactions.add(transactions);
+
         print(
-            "Your account is debited with $amount on ${transactions.createDate} and total balance is ${account.balance}");
+            "Your account is debited with $amount on ${transaction.createDate} and total balance is ${account.balance}");
       }
     } else {
       print("----Your account is closed----");
+    }
+  }
+
+  // void viewTransactionHistory() {
+  //   print("your transactions : \n");
+  //   for (var item in account.transactions) {
+  //     String type = (item.type == credit ? "+" : "-");
+  //     print("${type}  ${item.amount}  \n");
+  //   }
+  void viewTransactionHistory() {
+    print("your transactions : \n");
+    for (var item in account.transactions) {
+      if (transaction.type == "credit") {
+        print(
+            "${transaction.type} -- ${transaction.amount} -- ${transaction.createDate}");
+      } else {
+        print(
+            "${transaction.type} -- ${transaction.amount} -- ${transaction.createDate}");
+      }
     }
   }
 
@@ -88,9 +117,9 @@ class Bank {
         print("enter the account number");
         account.accountNumber = int.parse(stdin.readLineSync()!);
       }
-      print("Enter your new name");
-      account.name = stdin.readLineSync()!;
-      print("----Your Profile is Updated----");
+      //   print("Enter your new name");
+      //   account.name = stdin.readLineSync()!;
+      //   print("----Your Profile is Updated----");
     } else {
       print("----Your account is closed----");
     }
@@ -100,7 +129,7 @@ class Bank {
     account.accountNumber = 0;
     account.name = "";
     account.balance = 0;
-    account.status = false;
+    // status = false;
     // transactions.amount = 0;
     print("----Your account is closed----");
   }
